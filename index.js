350931d23eeb3143e737255bfe50f1ef5fccfd26
ioncreature <it.is.amarenin@gmail.com>
2019-08-26 13:12:36 +0300

    added empty files

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
 'use strict';
 
+exports.Broker = require('./src/broker');
+exports.Service = require('./src/service');
+exports.Singleton = require('./src/singleton');
+exports.Plugin = require('./src/action');
+exports.Action = require('./src/action');
+exports.Handler = require('./src/handler');

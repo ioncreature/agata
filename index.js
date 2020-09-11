c7aff05400206298f3a14c6764470e5bc795f984
ioncreature <it.is.amarenin@gmail.com>
2020-09-11 22:11:12 +0400

    chore: added a bit of types info, v2.3.4

--- index.js
+++ index.js
@@ -8,8 +8,32 @@ const
     Action = require('./src/action');
 
 
+/**
+ * @param {Object} config
+ * @return {Broker}
+ */
 exports.Broker = config => new Broker(config);
+
+/**
+ * @param {Object} config
+ * @return {Service}
+ */
 exports.Service = config => new Service(config);
+
+/**
+ * @param {Object} config
+ * @return {Singleton}
+ */
 exports.Singleton = config => new Singleton(config);
+
+/**
+ * @param {Object} config
+ * @return {Plugin}
+ */
 exports.Plugin = config => new Plugin(config);
+
+/**
+ * @param {Object} config
+ * @return {Action}
+ */
 exports.Action = config => new Action(config);
